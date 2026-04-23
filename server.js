2a77c234e5e23b3bd54e48a883945376b9e9006d
Shruti Jain <shrutijain1155@gmail.com>
2026-04-23 14:01:58 +0530

    Changes in server

--- server.js
+++ server.js
@@ -11,10 +11,13 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type']
 }));
+
+// Handle preflight requests globally
+app.options('*', cors());
 app.use(express.json());
 
 // Test Route
